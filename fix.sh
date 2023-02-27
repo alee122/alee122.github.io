0b086cfe83b7b0c618cf4ee358516b9d058a364b
Alice Lee <Alice.Lee@ll.mit.edu>
2023-02-28 00:40:40 +0100

    italy trip, and maybe I broke stuff? pushing to see if it works

--- fix.sh
+++ fix.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-mogrify -resize 2400x3600\> -quality 80 -sampling-factor 4:2:0 $1
+for i in *.jpg; do
+    [ -f "$i" ] || break
+    mogrify -resize 2400x3600\> -quality 80 -sampling-factor 4:2:0 $i
+done
